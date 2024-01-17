7bead8d7c95c459265d16d699c21d4c9dba9164e
CHiggins16 <135843000+CHiggins16@users.noreply.github.com>
2024-01-17 13:59:17 +0000

    Add files via upload

--- ORCiD API App/Flask_Tutorial.py
+++ ORCiD API App/Flask_Tutorial.py
@@ -47,7 +47,7 @@ def load_json_file(file_path):
 @app.route('/api/oauth2', methods=['GET', 'POST'])
 def get_oauth_data2():
     # Access token for the ORCiD API
-    access_token = 'db8a13cb-49cb-418e-8004-6b4b9d1beea6'
+    access_token = '80e9d501-fbe2-46d2-abcb-d196f1f7ba86'
 
     # URL for the GET request
     url = 'https://pub.sandbox.orcid.org/v3.0/0009-0006-4684-0381/educations'
@@ -79,6 +79,27 @@ def get_oauth_data2():
     else:
         # Print an error message if the request was not successful
         print(f'Error: {response.status_code} - {response.text}')
+
+@app.route('/index')
+def index():
+    return render_template('index2.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    # Access the form data using request.form
+    input_data = request.form['input_data']
+
+    # Convert the data to a Python variable
+    # For example, you can simply assign it to a variable
+    python_variable = input_data
+
+    # Now you can do whatever you want with the Python variable
+    # In this example, we'll just print it
+    print("Input data:", python_variable)
+
+    # You can also pass the variable to another template or redirect to another page
+    # For simplicity, let's just render a template with the variable
+    return render_template('result.html', python_variable=python_variable)
 
 if __name__ == "__main__":
     app.run(debug=True)
