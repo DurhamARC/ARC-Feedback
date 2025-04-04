689171258e12eb6db03106f8c7f7bdbaf96995db
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-04-04 13:49:58 +0100

    Basic production dockerfile for Azure deployment

---
+++ conf/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Starting SSH ..."
+service ssh start
+
+echo "Starting nginx..."
+nginx -t
+touch /var/log/nginx/access.log /var/log/nginx/error.log
+ln -sf /dev/stdout /var/log/nginx/access.log
+ln -sf /dev/stderr /var/log/nginx/error.log
+service nginx start
+
+cd /app/SearchApp
+
+# Add `--access-logfile '-' --log-level=debug` for debugging
+gunicorn --workers 2 --timeout=20 \
+         --access-logfile '-' --log-level=debug \
+         --log-file=- --bind=127.0.0.1:5000 wsgi:app
